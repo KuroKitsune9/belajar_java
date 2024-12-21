06aad1c18d0dd7a211a5e961226e4a3fd9be6ce9
KuroKitsune <dealgendut@gmail.com>
2024-12-21 15:37:22 +0700

    Coba

---
+++ CobaCoba.java
@@ -0,0 +1,10 @@
+public class CobaCoba {
+    public static void main(String[] args) {
+        System.out.println("contoh array size");
+        System.out.println("->");
+
+        int[] array_integer = { 1, 2, 3, 4, };
+
+        System.out.println(array_integer[1]);
+    }
+}
